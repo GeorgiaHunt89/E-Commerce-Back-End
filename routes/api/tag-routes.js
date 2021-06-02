68f0dff8d0a984314a3dd227c4c30d17ec3a2989
Georgia Hunt <georgialhunt89@gmail.com>
2021-06-02 16:00:26 +0930

    Added Get router to find all tags

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,10 +3,29 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+// Get router to find all tags
+router.get('/', async (req, res) => {
+  try {
+    const tagData = await Tag.findAll({
+    // Includes associated Products data
+      include: [
+        {
+          model: Product,
+          attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+        }
+      ]
+    });
+    if (!tagData) {
+      res.status(404).json({ message: 'No tags found' });
+      return;
+    }
+
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
+  
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
