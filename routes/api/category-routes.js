2a973012d2c4708f6fb22ec1d3bab4739bcaf74d
Georgia Hunt <georgialhunt89@gmail.com>
2021-06-02 13:54:15 +0930

    Added category get by id router

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -26,9 +26,27 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
-  // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+    // be sure to include its associated Products   
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+      }
+    ]
+    });
+    if (!categoryData) {
+      res.status(404).json({ message: 'No categories found with this id!' });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
