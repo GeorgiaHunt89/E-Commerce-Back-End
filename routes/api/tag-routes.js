c3af6561b9eb227ae9aed1b404ce6a771f079f6a
Georgia Hunt <georgialhunt89@gmail.com>
2021-06-02 16:02:54 +0930

    Added Get router to find single tag by id

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -26,10 +26,27 @@ router.get('/', async (req, res) => {
   }
 });
   
+// Get router to find single tag by its `id` value
+router.get('/:id', async (req, res) => {
+  try {
+    const tagData = await Tag.findByPk(req.params.id, {
+    // Includes associated Products data
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+      }
+    ]
+    });
+    if (!tagData) {
+      res.status(404).json({ message: 'No tags found with this id!' });
+      return;
+    }
 
-router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
